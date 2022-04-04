40281888c54634cf9077e4322e4d03648adc86bb
giiovananeves <giovana.silva23@fatec.sp.gov.br>
2022-04-04 17:03:53 +0000

    Resolução questão 2

---
+++ TodososMultiplos.java
@@ -0,0 +1,15 @@
+public class TodososMultiplos {
+    
+    public static void main(String [] args) {
+        for ( int i = 0 ; i < 100 ; i++ ) {
+            if (i%6==0){
+                System.out.println(i);
+                
+            }
+        }
+
+
+
+    }
+
+}
